556fb412ad149fa8284c3f20ceb504dbe70f35f5
Patryk Krzyżak <p.krzyzak1986@gmail.com>
2024-01-05 12:02:34 +0100

    22.3 Client Development

--- src/main/java/com/crud/tasks/domain/Badges.java
+++ src/main/java/com/crud/tasks/domain/Badges.java
@@ -4,16 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
+public class Badges {
 
-    @JsonProperty("name")
-    private String name;
-    @JsonProperty("id")
-    private String id;
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
+    private int votes;
+    @JsonProperty("attachmentsByType")
+    private AttachmentsByType attachments;
 }
