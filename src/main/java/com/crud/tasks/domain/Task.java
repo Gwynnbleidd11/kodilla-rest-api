105b698fd37bfc1a0ac2c8574faca7ffb53ecea0
Gwynnbleidd11 <140753432+Gwynnbleidd11@users.noreply.github.com>
2024-01-12 08:45:34 +0000

    Correction of files

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -1,12 +1,5 @@
 package com.crud.tasks.domain;
 
-<<<<<<< HEAD
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-=======
->>>>>>> 18790db42f7b23ca0213c3bd5d90da4dd3ba9a4d
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
